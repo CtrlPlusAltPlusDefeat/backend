ce23b00e31365d5aa9782f65ceeb5eaf1d3e7698
Env-byte <46604415+Env-byte@users.noreply.github.com>
2023-04-14 15:07:35 +0100

    Created go.mod (#1)
    Create go.mod

---
+++ pkg/handlers/test-handler/test-handler.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"backend/pkg/aws-helpers"
+	dynamodb2 "backend/pkg/aws-helpers/dynamodb"
+	"encoding/json"
+	"fmt"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+)
+
+var dbClient *dynamodb.Client
+var tableBasics dynamodb2.TableBasics
+
+type LambdaResponse struct {
+	Body string `json:"body"`
+}
+
+type TableList struct {
+	Tables []string `json:"tables"`
+}
+
+func HandleLambdaEvent() (LambdaResponse, error) {
+	fmt.Println("Entered HandleLambdaEvent")
+
+	tables, err := tableBasics.ListTables()
+	if err != nil {
+		return LambdaResponse{}, err
+	}
+
+	tableList := TableList{Tables: tables}
+	encodedJson, _ := json.Marshal(tableList)
+
+	return LambdaResponse{Body: fmt.Sprintf("%s", encodedJson)}, nil
+}
+
+// init is called when lambda is booted up
+func init() {
+	dbClient = dynamodb.NewFromConfig(aws_helpers.GetConfig())
+	tableBasics = dynamodb2.TableBasics{DynamoDbClient: dbClient}
+	_, err := tableBasics.CreateTable("TestTable-Code")
+	if err != nil {
+		fmt.Print(err)
+	}
+}
+
+func main() {
+	lambda.Start(HandleLambdaEvent)
+}
