44f139ef8064eb0056cecf0f3b0f13d363efdabd
Matthew Sutton <matthewsutton97@outlook.com>
2023-04-28 11:32:10 +0100

    Dynamo db access (#32)
    * Trying to set up secret retrieval and db access from lambda.
    
    * Comma?
    
    * Adding logs to see where it actually gets to.
    
    * Update config.go

--- pkg/aws-helpers/config.go
+++ pkg/aws-helpers/config.go
@@ -7,6 +7,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-secretsmanager-caching-go/secretcache"
 	"os"
+	"log"
 )
 
 var (
@@ -14,12 +15,19 @@ var (
 )
 
 func GetConfig() aws.Config {
+	log.Printf("GetConfig #1")
+
 	dbUrl := os.Getenv("DYNAMO_DB_URL")
 
+	log.Printf("GetConfig #2")
+
 	if len(dbUrl) > 0 {
+		log.Printf("GetConfig #3 - Local")
 		return getLocalConfig()
 	}
 
+	log.Printf("GetConfig #3 - Prod")
+  
 	return getProductionConfig()
 }
 
@@ -54,20 +62,32 @@ func getLocalConfig() aws.Config {
 	return cfg
 }
 
-
 func getProductionConfig() aws.Config {
+	log.Printf("GetConfig #5")
+  
 	key, _ := secretCache.GetSecretString("BackendAccessKey")
+  
+	log.Printf("GetConfig #6")
+  
 	secret, _ := secretCache.GetSecretString("BackendSecretAccessKey")
+
+	log.Printf("GetConfig #7")
 
 	cfg, err := config.LoadDefaultConfig(context.TODO(),
 		config.WithRegion("eu-west-1"),
 		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(key, secret, "")),
 	)
 
+	log.Printf("GetConfig #8")
+
 	if err != nil {
+		log.Printf("GetConfig #9")
+    
 		//we panic here because this is a fatal error, we cannot continue from this
 		panic(err)
 	}
 	
+	log.Printf("GetConfig #10")
+  
 	return cfg
 }
