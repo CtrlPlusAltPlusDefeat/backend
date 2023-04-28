82999c13b2e456af7d0c33092ce6063e09a982a1
Matthew Sutton <matthewsutton97@outlook.com>
2023-04-28 14:34:02 +0100

    Clean up now we are able to connect to required services. (#36)

--- pkg/aws-helpers/config.go
+++ pkg/aws-helpers/config.go
@@ -6,23 +6,15 @@ import (
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
-	"log"
 	"os"
 )
 
 func GetConfig() aws.Config {
-	log.Printf("GetConfig #1")
-
 	dbUrl := os.Getenv("DYNAMO_DB_URL")
 
-	log.Printf("GetConfig #2")
-
 	if len(dbUrl) > 0 {
-		log.Printf("GetConfig #3 - Local")
 		return getLocalConfig()
 	}
-
-	log.Printf("GetConfig #3 - Prod")
 
 	return getProductionConfig()
 }
@@ -59,63 +51,37 @@ func getLocalConfig() aws.Config {
 }
 
 func getProductionConfig() aws.Config {
-	log.Printf("GetConfig #5")
-
 	defaultConfig, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("eu-west-2"))
-
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
-	log.Printf("GetConfig #6")
-
 	svc := secretsmanager.NewFromConfig(defaultConfig)
-
-	log.Printf("GetConfig #7")
 
 	result, err := svc.GetSecretValue(context.TODO(), &secretsmanager.GetSecretValueInput{
 		SecretId: aws.String("BackendAccessKey"),
 	})
-
 	if err != nil {
-		log.Fatal(err.Error())
+		panic(err)
 	}
-
-	log.Printf("GetConfig #8")
-
-	var key string = *result.SecretString
-
-	log.Printf("GetConfig #9")
 
 	result, err = svc.GetSecretValue(context.TODO(), &secretsmanager.GetSecretValueInput{
 		SecretId: aws.String("BackendSecretAccessKey"),
 	})
-
 	if err != nil {
-		log.Fatal(err.Error())
+		panic(err)
 	}
 
-	log.Printf("GetConfig #10")
-
+	var key string = *result.SecretString
 	var secret string = *result.SecretString
-
-	log.Printf("GetConfig #11")
 
 	cfg, err := config.LoadDefaultConfig(context.TODO(),
 		config.WithRegion("eu-west-2"),
 		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(key, secret, "")),
 	)
-
-	log.Printf("GetConfig #12")
-
 	if err != nil {
-		log.Printf("GetConfig #13")
-		log.Printf(err.Error())
-
 		panic(err)
 	}
-
-	log.Printf("GetConfig #14")
 
 	return cfg
 }
