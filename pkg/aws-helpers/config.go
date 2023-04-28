c65c9a8f3f2ae22aa10dfb637f2f84e20b14d89e
Matthew Sutton <matthewsutton97@outlook.com>
2023-04-28 13:16:53 +0100

    Dynamo db access (#34)
    * Trying to set up secret retrieval and db access from lambda.
    
    * Comma?
    
    * Adding logs to see where it actually gets to.
    
    * Update config.go
    
    * MOre logging.
    
    * Update config.go
    
    * Update config.go
    
    * Update config.go
    
    * Dont log errors if they don't exist because that would never work.

--- pkg/aws-helpers/config.go
+++ pkg/aws-helpers/config.go
@@ -6,8 +6,8 @@ import (
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-secretsmanager-caching-go/secretcache"
-	"os"
 	"log"
+	"os"
 )
 
 func GetConfig() aws.Config {
@@ -23,7 +23,7 @@ func GetConfig() aws.Config {
 	}
 
 	log.Printf("GetConfig #3 - Prod")
-  
+
 	return getProductionConfig()
 }
 
@@ -62,19 +62,25 @@ func getProductionConfig() aws.Config {
 	log.Printf("GetConfig #5")
 
 	secretCache, err1 := secretcache.New()
-  
+
 	log.Printf("GetConfig #6")
-	log.Printf(err1.Error())
+	if err1 != nil {
+		log.Printf(err1.Error())
+	}
 
 	key, err2 := secretCache.GetSecretString("BackendAccessKey")
-  
+
 	log.Printf("GetConfig #7")
-	log.Printf(err2.Error())
-  
+	if err2 != nil {
+		log.Printf(err2.Error())
+	}
+
 	secret, err3 := secretCache.GetSecretString("BackendSecretAccessKey")
 
 	log.Printf("GetConfig #8")
-	log.Printf(err3.Error())
+	if err3 != nil {
+		log.Printf(err3.Error())
+	}
 
 	cfg, err := config.LoadDefaultConfig(context.TODO(),
 		config.WithRegion("eu-west-1"),
@@ -82,17 +88,18 @@ func getProductionConfig() aws.Config {
 	)
 
 	log.Printf("GetConfig #9")
-	log.Printf(err.Error())
+	if err != nil {
+		log.Printf(err.Error())
+	}
 
 	if err != nil {
 		log.Printf("GetConfig #10")
-    
+
 		//we panic here because this is a fatal error, we cannot continue from this
 		panic(err)
 	}
-	
+
 	log.Printf("GetConfig #11")
-  
+
 	return cfg
 }
-
