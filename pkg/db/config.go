c46cedbee98a81d480bac58ffb6437da41f8b1b3
Matthew Sutton <matthewsutton97@outlook.com>
2023-04-28 18:49:29 +0100

    Moving db config into common func.

--- pkg/db/config.go
+++ pkg/db/config.go
@@ -1,5 +1,15 @@
 package db
 
-import "github.com/aws/aws-sdk-go-v2/service/dynamodb"
+import (
+	helpers "backend/pkg/aws-helpers"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+)
 
 var DynamoDb *dynamodb.Client
+
+func Configure() {
+	config := helpers.GetConfig()
+	client := dynamodb.NewFromConfig(config)
+
+	DynamoDb = client
+}
