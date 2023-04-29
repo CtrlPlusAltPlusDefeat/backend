9e286ab2fedc36e2c1814aaf22ef596557d551b8
Matthew Sutton <matthewsutton97@outlook.com>
2023-04-29 07:24:00 +0100

    Remove cyclic depdency.

--- pkg/routes/routes.go
+++ pkg/routes/routes.go
@@ -2,7 +2,6 @@ package routes
 
 import (
 	"backend/pkg/models"
-	"backend/pkg/routes/middleware"
 	"backend/pkg/services"
 	"log"
 )
@@ -18,13 +17,13 @@ var (
 )
 
 func Configure() {
-	add("player|create-session", services.CreateSession, middleware.ErrorCommunicateMiddleware)
-	add("player|use-session", services.UseSession, middleware.ErrorCommunicateMiddleware)
-	add("chat|send", services.SendChat, middleware.SessionMiddleware)
-	add("lobby|create", services.CreateLobby, middleware.SessionMiddleware)
-	add("lobby|join", services.JoinLobby, middleware.SessionMiddleware)
-	add("lobby|set-name", services.SetLobbyName, middleware.SessionMiddleware)
-	add("lobby|get", services.GetLobby, middleware.SessionMiddleware)
+	add("player|create-session", services.CreateSession, ErrorCommunicateMiddleware)
+	add("player|use-session", services.UseSession, ErrorCommunicateMiddleware)
+	add("chat|send", services.SendChat, SessionMiddleware)
+	add("lobby|create", services.CreateLobby, SessionMiddleware)
+	add("lobby|join", services.JoinLobby, SessionMiddleware)
+	add("lobby|set-name", services.SetLobbyName, SessionMiddleware)
+	add("lobby|get", services.GetLobby, SessionMiddleware)
 }
 
 func add(route string, handler Handler, middleware ...Middleware) {
